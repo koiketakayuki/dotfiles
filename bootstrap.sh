c978d25487bfe2a0b8903ce6e511c406e750be56
koiketakayuki <koiketakayuki@koiketakayukis-MacBook-Pro.local>
2019-07-07 22:29:13 +0900

    use whitespace instead of tab

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,15 +7,15 @@ function setup() {
 
 function copyConfigFiles() {
     rsync --exclude ".git" \
-	  --exclude "bootstrap.sh" \
-      --exclude "LICENSE" \
-      --exclude "README.md" \
-	  -avh --no-perms . ~;
+          --exclude "bootstrap.sh" \
+          --exclude "LICENSE" \
+          --exclude "README.md" \
+          -avh --no-perms . ~;
 }
 
 function brewInstall() {
     command -v brew > /dev/null 2>&1 || {
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     }
     brew update;
     brew bundle --global;
@@ -24,13 +24,13 @@ function brewInstall() {
 
 function main() {
     if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	setup;
+        setup;
     else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n)" yn;
-	case $yn in
-	    [Yy]* ) setup ;;
+        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n)" yn;
+        case $yn in
+            [Yy]* ) setup ;;
             * ) exit;;
-	esac
+        esac
     fi;
 }
 
